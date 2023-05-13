91a750ab179359261d115dc2a54ad2675c1e2b2d
Afsaneh Karami <78735911+Afsaneh-Karami@users.noreply.github.com>
2023-05-13 21:26:21 +0100

    Add files via upload

---
+++ image segmentation(watershed)/task1a_activecon.m
@@ -0,0 +1,50 @@
+clc;
+close all;
+similarity1=zeros(16,1);
+similarity2=zeros(16,1);
+for i=1:16
+    if i<10
+        file_name=['plant00',num2str(i)];
+    else
+        file_name=['plant0',num2str(i)];
+    end
+    img=imread([file_name,'_rgb.png']);
+    g_truth=imread([file_name,'_label.png']);
+    gray_img=rgb2gray(img);
+    mask=zeros(size(gray_img));
+    mask(10:end-10,10:end-10)=255;
+    plant_segment = activecontour(gray_img,mask,150);
+    plant_segment =bwareaopen(plant_segment ,60);
+    ImageFolder='D:\semester B\computer vision\assessement\task 1\plant_segment\';
+    imwrite(plant_segment,[ImageFolder,file_name,'_plant_seg_active.png'])
+    Bw_g_truth=g_truth>0;
+    bw=createMask(img);
+    imwrite(bw,[ImageFolder,file_name,'_plant_seg_mask.png'])
+    imwrite(Bw_g_truth,[ImageFolder,file_name,'_gt1.png'])
+    similarity1(i)=dice(plant_segment,Bw_g_truth);
+    similarity2(i)=dice(bw,Bw_g_truth);
+     
+end
+similarity=[similarity1 similarity2];
+mean_sim1=mean(similarity1);
+mean_sim2=mean(similarity2);
+figure,bar(similarity,'grouped')
+xlabel('number of image')
+ylabel('dice value')
+legend('acti', 'mask','Location','bestoutside')
+disp(['average of dice for active contour methos is: ',num2str(mean_sim1)])
+disp(['average of dice for mask methos is: ',num2str(mean_sim2)])
+
+% img11=imread("D:\semester B\computer vision\assessement\task 1\plant002_rgb.png")
+% figure,
+% subplot(1,3,1)
+% imshow(img11)
+% title('origional');
+% img12=imread("D:\semester B\computer vision\assessement\task 1\plant_segment\plant002_plant_seg_active.png")
+% subplot(1,3,2)
+% imshow(img12)
+% title('active');
+% img13=imread("D:\semester B\computer vision\assessement\task 1\plant_segment\plant002_plant_seg_mask.png")
+% subplot(1,3,3)
+% imshow(img13)
+% title('mask');
---
+++ image segmentation(watershed)/task1c_watershed.m
@@ -0,0 +1,58 @@
+clc;
+close all;
+leaf_count=zeros(16,1);
+for i=1:16
+    if i<10
+        file_name=['plant00',num2str(i)];
+    else
+        file_name=['plant0',num2str(i)];
+    end
+    
+    img=imread([file_name,'_rgb.png']);
+    I1= rgb2gray(img);
+    level=graythresh(I1);
+    I=imbinarize(I1,level);
+    figure;
+    subplot(2,2,1);
+    imshow(img)
+    title('RGB image')
+    se = strel('disk',3);
+    se1 = strel('disk',1);
+    Ie = imopen(I,se);
+    Ie1=imerode(Ie,se1);
+    Ie3=edge(Ie1,"canny");
+    Ie4=Ie1+Ie3;
+    Ie5=bwareaopen(Ie4,20);
+    subplot(2,2,2);
+    imshow(Ie5)
+    title('BW image')
+    D=bwdist(~Ie5,"quasi-euclidean");
+    L=watershed(-D,8);
+    subplot(2,2,3);
+    imshow(-D,[])
+    title('bwdist')
+    L(~Ie5)=0;
+    out = bwareaopen(L ~= 0, 20);
+    Lfinal = zeros(size(L));
+    Lfinal(out) = L(out);
+    CC = bwconncomp(Lfinal,4);
+    L1 = labelmatrix(CC);
+    rgb=label2rgb(L1,'jet',[0.5,0.5,0.5]);
+    subplot(2,2,4);
+    imshow(rgb)
+    title(['num of leaves:',num2str(max(max(L1)))])
+    leaf_count(i)=(max(max(L1)));
+    filename = fullfile('D:\semester B\computer vision\assessement\task 1\leaf_segmentation\', [file_name,'_leafseg.png']);
+    saveas(gcf, filename, 'png');
+end
+Leaf_real_number = xlsread( 'Leaf_counts.csv', 'B:B' );
+leaf_data=[leaf_count Leaf_real_number];
+figure,bar(leaf_data,'grouped')
+xlabel('number of image')
+ylabel('number of leaves')
+legend('count', 'GT','Location','bestoutside')
+leaf_diff=abs(Leaf_real_number-leaf_count);
+mean_diff=mean(leaf_diff);
+tabel=table(leaf_diff,leaf_count,Leaf_real_number);
+disp(['Average of diference between real number of leaves and counted by segmentation code is ' ,num2str(mean_diff)])
+
